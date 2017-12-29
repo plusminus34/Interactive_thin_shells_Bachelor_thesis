ce1e5f8e1c5c93604336958394970e9ee534f346
Moritz Geilinger <moritzge@inf.ethz.ch>
2017-12-29 20:05:13 +0100

    Remove vertical GRF upper bound & seperate GRF vertical/tangential bound constraints

--- RobotDesignerLib/include/RobotDesignerLib/MPO_GRFSoftConstraints.h
+++ RobotDesignerLib/include/RobotDesignerLib/MPO_GRFSoftConstraints.h
@@ -5,6 +5,7 @@
 #include <RobotDesignerLib/LocomotionEngineMotionPlan.h>
 #include <OptimizationLib/SoftUnilateralConstraint.h>
 
+// This regularizer enforces GRF to vanish in swing (contactFlag = 0)
 class MPO_GRFSwingRegularizer : public ObjectiveFunction {
 public:
 	MPO_GRFSwingRegularizer(LocomotionEngineMotionPlan* mp, const std::string& objectiveDescription, double weight);
@@ -20,6 +21,7 @@ private:
 	LocomotionEngineMotionPlan* theMotionPlan;
 };
 
+// This regularizer encourages GRF to be small in stance (contactFlag = 1)
 class MPO_GRFStanceRegularizer : public ObjectiveFunction {
 public:
 	MPO_GRFStanceRegularizer(LocomotionEngineMotionPlan* mp, const std::string& objectiveDescription, double weight);
@@ -35,11 +37,25 @@ private:
 	LocomotionEngineMotionPlan* theMotionPlan;
 };
 
-
-class MPO_GRFSoftBoundConstraints : public ObjectiveFunction {
+class MPO_GRFTangentialBoundConstraints : public ObjectiveFunction {
 public:
-	MPO_GRFSoftBoundConstraints(LocomotionEngineMotionPlan* mp, const std::string& objectiveDescription, double weight);
-	virtual ~MPO_GRFSoftBoundConstraints(void);
+	MPO_GRFTangentialBoundConstraints(LocomotionEngineMotionPlan* mp, const std::string& objectiveDescription, double weight);
+	virtual ~MPO_GRFTangentialBoundConstraints(void);
+
+	virtual double computeValue(const dVector& p);
+
+	virtual void addHessianEntriesTo(DynamicArray<MTriplet>& hessianEntries, const dVector& p);
+	virtual void addGradientTo(dVector& grad, const dVector& p);
+
+private:
+	//the energy function operates on a motion plan...
+	LocomotionEngineMotionPlan* theMotionPlan;
+};
+
+class MPO_GRFVerticalLowerBoundConstraints : public ObjectiveFunction {
+public:
+	MPO_GRFVerticalLowerBoundConstraints(LocomotionEngineMotionPlan* mp, const std::string& objectiveDescription, double weight);
+	virtual ~MPO_GRFVerticalLowerBoundConstraints(void);
 
 	virtual double computeValue(const dVector& p);
 
