745d9773eb60f6a429a696f33779a3d9f2fecd19
Stelian Coros <scoros@inf.ethz.ch>
2018-02-01 00:00:54 +0100

    slight renaming.

--- RobotDesignerLib/src/LivingBracket.cpp
+++ RobotDesignerLib/src/LivingBracket.cpp
@@ -141,3 +141,129 @@ void Motor_RMC_HornBracket_BK3002::setDefaultFeaturePointList() {
 	featurePoints.push_back(P3D(-lenX, 0.02, -lenZ));
 	featurePoints.push_back(P3D(-lenX, 0.02, lenZ));
 }
+
+/*****************************************************************************************/
+
+Motor_RMC_BodyBracket::Motor_RMC_BodyBracket() {
+
+}
+
+Motor_RMC_BodyBracket::~Motor_RMC_BodyBracket() {
+
+}
+
+void Motor_RMC_BodyBracket::draw() {
+	if (bodyBracketMesh)
+		bodyBracketMesh->drawMesh();
+}
+
+void Motor_RMC_BodyBracket::setColor(const Vector4d& color /*= Vector4d(0, 0, 0, 0)*/){
+	if (color.isZero()){
+		bodyBracketMesh->setMaterial(bodyBracketMaterial);
+	}
+	else {
+		GLShaderMaterial defaultMat;
+		defaultMat.setColor(color[0], color[1], color[2], color[3]);
+		bodyBracketMesh->setMaterial(defaultMat);
+	}
+}
+
+Motor_RMC_BodyBracket_XM430::Motor_RMC_BodyBracket_XM430() {
+//	bodyBracketMesh = GLContentManager::getGLMesh("../data/robotDesigner/meshes/XM-430_bodyBracket_w.obj"); bodyBracketMesh->getMaterial().setColor(0.7, 0.7, 0.7, 1.0);
+	bodyBracketMesh = GLContentManager::getGLMesh("../data/robotDesigner/meshes/XM-430_bodyBracket_lo.obj"); bodyBracketMesh->getMaterial().setColor(0.7, 0.7, 0.7, 1.0);
+
+	string whiteMat = "../data/textures/matcap/whitefluff2.bmp";
+	bodyBracketMaterial.setShaderProgram(GLContentManager::getShaderProgram("matcap"));
+	bodyBracketMaterial.setTextureParam(whiteMat.c_str(), GLContentManager::getTexture(whiteMat.c_str()));
+
+	generateBracketMesh();
+}
+
+Motor_RMC_BodyBracket_XM430::~Motor_RMC_BodyBracket_XM430() {
+
+}
+
+void Motor_RMC_BodyBracket_XM430::generateBracketMesh(){
+	pinInfos.clear();
+
+	P3D center = P3D(0, -0.038, 0);
+	double lenX = 0.0175;
+	double lenZ = 0.0205;
+
+	Transformation pinTrans(getRotationQuaternion(RAD(180), V3D(1, 0, 0)).getRotationMatrix(), V3D(center));
+
+	vector<P3D> FPs;
+	FPs.push_back(center + P3D(lenX, 0, lenZ));
+	FPs.push_back(center + P3D(-lenX, 0, lenZ));
+	FPs.push_back(center + P3D(-lenX, 0, -lenZ));
+	FPs.push_back(center + P3D(lenX, 0, -lenZ));
+
+	pinInfos.push_back(PinInfo(pinTrans, "BottomBracketPin", FPs, center, V3D(0, -1, 0)));
+}
+
+
+
+Motor_RMC_BodyBracket_TGY306G::Motor_RMC_BodyBracket_TGY306G() {
+	bodyBracketMesh = GLContentManager::getGLMesh("../data/robotDesigner/meshes/TGY306G_bodyBracket_w.obj"); bodyBracketMesh->getMaterial().setColor(0.7, 0.7, 0.7, 1.0);
+
+	string whiteMat = "../data/textures/matcap/whitefluff2.bmp";
+	bodyBracketMaterial.setShaderProgram(GLContentManager::getShaderProgram("matcap"));
+	bodyBracketMaterial.setTextureParam(whiteMat.c_str(), GLContentManager::getTexture(whiteMat.c_str()));
+
+	generateBracketMesh();
+}
+
+Motor_RMC_BodyBracket_TGY306G::~Motor_RMC_BodyBracket_TGY306G() {
+
+}
+
+void Motor_RMC_BodyBracket_TGY306G::generateBracketMesh() {
+	pinInfos.clear();
+
+	P3D center = P3D(0, -0.024, 0);
+	double lenX = 0.008;
+	double lenZ = 0.012;
+
+	Transformation pinTrans(getRotationQuaternion(RAD(180), V3D(1, 0, 0)).getRotationMatrix(), V3D(center));
+
+	vector<P3D> FPs;
+	FPs.push_back(center + P3D(lenX, 0, lenZ /*/ 2*/));
+	FPs.push_back(center + P3D(-lenX, 0, lenZ /*/ 2*/));
+	FPs.push_back(center + P3D(-lenX, 0, -lenZ));
+	FPs.push_back(center + P3D(lenX, 0, -lenZ));
+
+	pinInfos.push_back(PinInfo(pinTrans, "BottomBracketPin", FPs, center, V3D(0, -1, 0)));
+}
+
+Motor_RMC_BodyBracket_BK3002::Motor_RMC_BodyBracket_BK3002() {
+	bodyBracketMesh = GLContentManager::getGLMesh("../data/robotDesigner/meshes/BK3002_bodyBracket_d.obj"); bodyBracketMesh->getMaterial().setColor(0.7, 0.7, 0.7, 1.0);
+
+	string whiteMat = "../data/textures/matcap/blackFluff.bmp";
+	bodyBracketMaterial.setShaderProgram(GLContentManager::getShaderProgram("matcap"));
+	bodyBracketMaterial.setTextureParam(whiteMat.c_str(), GLContentManager::getTexture(whiteMat.c_str()));
+
+	generateBracketMesh();
+}
+
+Motor_RMC_BodyBracket_BK3002::~Motor_RMC_BodyBracket_BK3002() {
+
+}
+
+void Motor_RMC_BodyBracket_BK3002::generateBracketMesh() {
+	pinInfos.clear();
+
+	P3D center = P3D(0, -0.024, 0);
+	double lenX = 0.008;
+	double lenZ = 0.012;
+
+	Transformation pinTrans(getRotationQuaternion(RAD(180), V3D(1, 0, 0)).getRotationMatrix(), V3D(center));
+
+	vector<P3D> FPs;
+	FPs.push_back(center + P3D(lenX, 0, lenZ /*/ 2*/));
+	FPs.push_back(center + P3D(-lenX, 0, lenZ /*/ 2*/));
+	FPs.push_back(center + P3D(-lenX, 0, -lenZ));
+	FPs.push_back(center + P3D(lenX, 0, -lenZ));
+
+	pinInfos.push_back(PinInfo(pinTrans, "BottomBracketPin", FPs, center, V3D(0, -1, 0)));
+}
+
