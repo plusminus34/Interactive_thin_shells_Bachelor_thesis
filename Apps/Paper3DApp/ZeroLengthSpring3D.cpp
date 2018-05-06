3c7490b133107d63b8b89e42cedeaf8676e8175e
Linus Wigger <wiggerl@student.ethz.ch>
2018-05-06 15:39:28 +0200

    Add ZeroLengthSpring3D for pinning nodes together

---
+++ Apps/Paper3DApp/ZeroLengthSpring3D.cpp
@@ -0,0 +1,66 @@
+#include "ZeroLengthSpring3D.h"
+
+ZeroLengthSpring3D::ZeroLengthSpring3D(SimulationMesh* simMesh, Node* n1, Node* n2) : SimMeshElement(simMesh) {
+
+    this->n[0] = n1;
+    this->n[1] = n2;
+
+	computeHessianComponents();
+}
+
+ZeroLengthSpring3D::~ZeroLengthSpring3D() {
+}
+
+double ZeroLengthSpring3D::getMass() {
+	return 0;
+}
+
+V3D ZeroLengthSpring3D::getCurrentEdgeVector(const dVector& x, const dVector& X) {
+	return V3D(n[1]->getCoordinates(x), n[0]->getCoordinates(x));
+}
+
+double ZeroLengthSpring3D::getCurrentEdgeLength(const dVector& x, const dVector& X) {
+	return getCurrentEdgeVector(x, X).length();
+}
+
+double ZeroLengthSpring3D::getEnergy(const dVector& x, const dVector& X) {
+	double l = getCurrentEdgeLength(x, X);
+	return 0.5 * k * l * l;
+}
+
+void ZeroLengthSpring3D::addEnergyGradientTo(const dVector& x, const dVector& X, dVector& grad) {
+	//compute the gradient, and write it out
+	computeGradientComponents(x, X);
+	for (int i = 0; i<2; i++)
+		for (int j = 0; j<3; j++)
+			grad[n[i]->dataStartIndex + j] += dEdx[i][j];
+}
+
+void ZeroLengthSpring3D::addEnergyHessianTo(const dVector& x, const dVector& X, std::vector<MTriplet>& hesEntries) {
+	//Hessian blocks are constant and were prepared during initialization
+	for (int i = 0; i<2; i++)
+		for (int j = 0; j < 2; j++)
+			addSparseMatrixDenseBlockToTriplet(hesEntries, n[i]->dataStartIndex, n[j]->dataStartIndex, ddEdxdx[i][j], true);
+}
+
+void ZeroLengthSpring3D::draw(const dVector& x) {
+}
+
+void ZeroLengthSpring3D::drawRestConfiguration(const dVector& X) {
+}
+
+void ZeroLengthSpring3D::computeGradientComponents(const dVector& x, const dVector& X) {
+	V3D v = getCurrentEdgeVector(x, X);
+	dEdx[0] = v * k;
+	dEdx[1] = - v * k;
+}
+
+void ZeroLengthSpring3D::computeHessianComponents() {
+	V3D kkk(k, k, k);
+	for(int i=0;i<2;++i)
+		for (int j = 0; j < 2; ++j) {
+			ddEdxdx[i][j].setIdentity();
+			ddEdxdx[i][j] *= k;
+			if (i != j) ddEdxdx[i][j] *= -1;
+		}
+}
---
+++ Apps/Paper3DApp/ZeroLengthSpring3D.h
@@ -0,0 +1,37 @@
+#pragma once
+#include <FEMSimLib/BilateralSpring3D.h>
+
+/*
+	Spring that pins two nodes together
+*/
+class ZeroLengthSpring3D : public SimMeshElement {
+protected:
+	//material parameters...
+	double k = 500;
+
+	//the collection of nodes that define the edge element
+	Node* n[2];
+	//parameters needed for gradient and hessian of the energy
+	V3D dEdx[2];
+	Matrix3x3 ddEdxdx[2][2];
+
+	V3D getCurrentEdgeVector(const dVector& x, const dVector& X);
+	double getCurrentEdgeLength(const dVector& x, const dVector& X);
+
+	void computeGradientComponents(const dVector& x, const dVector& X);
+	void computeHessianComponents();
+
+	virtual double getMass();
+
+	virtual double getEnergy(const dVector& x, const dVector& X);
+	virtual void addEnergyGradientTo(const dVector& x, const dVector& X, dVector& grad);
+	virtual void addEnergyHessianTo(const dVector& x, const dVector& X, std::vector<MTriplet>& hesEntries);
+	virtual void draw(const dVector& x);
+	virtual void drawRestConfiguration(const dVector& X);
+
+public:
+	ZeroLengthSpring3D(SimulationMesh* simMesh, Node* n1, Node* n2);
+	~ZeroLengthSpring3D();
+
+};
+
